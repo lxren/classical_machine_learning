8f2b977e04e2771ff740b1de3acecf993ea61852
Lily Ren <lilyxjren@gmail.com>
2024-04-29 21:13:29 -0600

    initiate repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="Exploring classical machine learning techniques include Decision Tree, Nearest Neighbour, and Logistic Regression",
+    author='Lily Ren',
+    license='MIT',
+    install_requires=['pandas','numpy']
+)
---
+++ src/data/download_dataset.py
@@ -0,0 +1,8 @@
+import os
+from urllib.request import urlretrieve
+
+def download_dataset():
+    
+
+if __name__ == '__main__':
+    download_dataset()
