5f6234187ee638b929a8decb36737e4785ab5fc6
Lily Ren <lilyxjren@gmail.com>
2024-05-09 00:00:00 -0600

    build argparse in main to take multiple arguments from user to choose which model to build

---
+++ README.md
@@ -0,0 +1,21 @@
+### Install libraries
+```bash
+pip install .
+```
+### Download data for local copy (optional)
+```bash
+python src/data/download_dataset.py
+```
+### Build dataset (build directly from json file using mlcroissant)
+```bash
+python src/data/build_dataset.py
+```
+### Print & preview first 5 rows of dataset
+```bash
+python ./src/main.py --verbose
+```
+### Build Model (choose between DecisionTree, KNearestNeighbors, and LogisticRegression)
+```bash
+python ./src/main.py --model <model_name>
+model_name: DecisionTree|KNearestNeighbors|LogisticRegression
+```
--- src/main.py
+++ src/main.py
@@ -1,11 +1,37 @@
-from data.build_dataset import build_dataset
-from models.build_DecisionTree import build_DecisionTree
+import argparse
 import sys
 sys.dont_write_bytecode = True
+from data.build_dataset import build_dataset
+from models.build_DecisionTree import build_DecisionTree
 
 df = build_dataset()
 
-print(df.head())
+# build_DecisionTree(df)
 
-build_DecisionTree(df)
+if __name__ == '__main__':
 
+    # build argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--model', help=f"Select a model between 'DecisionTree','KNearestNeighbors','LogisticRegression'")
+    args = parser.parse_args()
+    model = args.model
+
+    # verbose to print df head
+    if args.verbose:
+        print(df.head())
+
+    # accept arg to select and run ML model
+    if not model:
+        model = input("Please select a model between 'DecisionTree','KNearestNeighbors','LogisticRegression'")
+    match model:
+        case "DecisionTree":
+            build_DecisionTree(df)
+        case "KNearestNeighbors":
+            print('KNN')
+        case "LogisticRegression":
+            print("LR")
+    
+    
+    #output = build_model(model)
+    #print(output)
