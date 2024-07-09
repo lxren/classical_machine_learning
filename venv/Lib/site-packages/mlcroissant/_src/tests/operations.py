d7273afd1f45c59c7853b5f885901576c270803c
Lily Ren <lilyxjren@gmail.com>
2024-07-09 17:35:17 -0600

    added knn model and catch case

--- venv/Lib/site-packages/mlcroissant/_src/tests/operations.py
+++
@@ -1,8 +0,0 @@
-"""Testing utils for `Operation`."""
-
-from mlcroissant._src.operation_graph.base_operation import Operations
-
-
-def operations() -> Operations:
-    """An empty graph of operations to be used in tests."""
-    return Operations()
--- venv/Lib/site-packages/mlcroissant/scripts/migrations/previous/202403271030.py
+++
@@ -1,6 +0,0 @@
-"""Migration: Standardizes @context."""
-
-
-def up(json_ld):
-    """Up migration that standardizes @context."""
-    return json_ld
